10c188e29e98c31e1ade3dad2e46a09866d8e2f2
felexkemboi <felokemboi10@gmail.com>
2019-06-08 03:02:01 +0300

    cleaning of my final code

--- crafttask.py
+++ crafttask.py
@@ -1,19 +1,13 @@
 
+#Import all the libraries to be used
 from airflow import DAG
+from airflow.models import DAG
 from airflow.operators.bash_operator import BashOperator
 from airflow.operators.python_operator import PythonOperator
-
-
-import datetime as dt
-from airflow.operators.python_operator import PythonOperator
-from airflow.models import DAG
-from datetime import datetime, timedelta
+from airflow.hooks.postgres_hook import PostgresHook
 from airflow.hooks.mysql_hook import MySqlHook
 from airflow.operators import MySqlOperator
-from datetime import datetime, timedelta
-from airflow import DAG
-from airflow.hooks.postgres_hook import PostgresHook
-from airflow.operators.python_operator import PythonOperator
+import datetime as dt
 from datetime import datetime, timedelta
 from psycopg2.extras import execute_values
 
@@ -24,8 +18,11 @@ default_args = {
     'retries': 5
 }
 
+#Query to fetch all records from Mysql
 sql = """ SELECT * FROM marks ;"""
 
+
+#once you have cloned the project,change the path of your greet.txt
 def greet():
     print('Writing in file')
     with open('/home/kemboi/Desktop/greet.txt', 'a+', encoding='utf8') as f:
@@ -35,6 +32,7 @@ def greet():
     return 'Greeted'
 
 
+#function to test things work as expected
 def respond():
     print("Hello just saying Hi")
 
@@ -42,14 +40,11 @@ def respond():
 
 def postgres(ds, **kwargs):
 
-
     #this should be a query to insert the data to the postgress
     query = """ SELECT *FROM marks """
 
     conn =  PostgresHook(postgres_conn_id='source', schema='marks').get_conn()
-    #dest_conn = PostgresHook(postgres_conn_id='dest',   schema='mak').get_conn()
 
-    # notice this time we are naming the cursor for the origin table
     # that's going to force the library to create a server cursor
     cursor = conn.cursor("serverCursor")
     cursor.execute(query)
@@ -57,6 +52,10 @@ def postgres(ds, **kwargs):
     # now we need to close our connection
     cursor.close()
 
+    return "Saved to the database"
+
+
+#these are the tasks to be scheduled 
 with DAG('craft_tasks', default_args=default_args, schedule_interval='*/1 * * * *') as dag:
     opr_hello = BashOperator(task_id='say_Hi',bash_command='echo "Hi!!"')
     opr_greet = PythonOperator(task_id='greet',python_callable=greet)
